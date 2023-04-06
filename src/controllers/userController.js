224aee6fc085507660ff0fc6541b9789fa6e228a
Rodrigo <rodrigolfranca@gmail.com>
2023-04-06 12:15:54 -0300

    add: finalização postNewUser

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 const userService = require('../services/userService.js')
+const bcrypt = require('bcrypt')
 import { validationResult } from 'express-validator'
 
 exports.getAllUsers = async (req, res) => {
@@ -40,7 +41,7 @@ exports.postUser = async (req, res) => {
     console.log("Controller User: Criando novo usuário")
     const errors = validationResult(req);
     if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
-
+    
 
     const user = {
         name: req.body.name,
@@ -48,7 +49,18 @@ exports.postUser = async (req, res) => {
         phone: req.body.phone,
         is_admin: false,
     };
-    user.password = await bcrypt.hashSync(req.body.password, 10);
+
+    try {
+
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+        user.password = hashedPassword;
+        
+    } catch (err) {
+
+        return res.status(500).json({message: "Controller Error: failed to process password, try again", err: err});
+
+    }
 
     try {
 
